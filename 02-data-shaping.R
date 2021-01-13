18c53abfed786c63dec2b0889b21a2c02746fa74
John Franchak <franchak@gmail.com>
2021-01-13 10:02:02 -0800

    added some data checking examples

--- 02-data-shaping.R
+++ 02-data-shaping.R
@@ -1,7 +1,6 @@
 #Load packages
 library(tidyverse)
 library(vroom)
-library(lubridate)
 library(here)
 
 col_names = c("trial_num","speed_actual","speed_response","correct")
@@ -14,3 +13,31 @@ header <- header %>%  pivot_wider(id_cols = file, names_from = field, values_fro
 ds_joined <- left_join(header, ds, by = "file")
 
 ds_joined <- ds_joined %>% separate(file, into = c(NA,NA, NA, "Participant_file","Block_file",NA))
+ds_joined <- ds_joined %>% rename_with(make_clean_names)
+
+speed_labels = c("slower", "faster")
+ds_joined <- ds_joined %>% 
+  mutate(
+    speed_actual = factor(speed_actual, levels = c("slo","fas"), labels = speed_labels),
+    speed_response = factor(speed_response, levels = c("slower","faster"), labels = speed_labels),
+  )
+
+checks <- ds_joined %>% 
+  group_by(participant, block) %>% 
+    summarize(
+      match_participant = sum(as.numeric(participant_file == participant)),
+      match_block = sum(as.numeric(block == block_file)),
+      match_response = sum((speed_actual == speed_response) == correct, na.rm = T),
+      trial_order = sum((trial_num - lag(trial_num, n = 1)), na.rm = T) + 1,
+      num_trials = n(),
+    )
+
+checks %>% filter(
+  !(match_participant == num_trials) | 
+  !(match_block == num_trials) |
+  !(match_response == num_trials) | 
+  !(trial_order == num_trials))
+
+ds_cleaned <- ds_joined %>% select(participant:correct)
+
+rm(list = c("ds", "header","ds_joined"))
