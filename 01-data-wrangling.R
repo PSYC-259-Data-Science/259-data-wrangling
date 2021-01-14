9167f9bfc3c0c9195fb9a07088e3acc2590c75a6
John Franchak <franchak@gmail.com>
2021-01-14 15:45:06 -0800

    updated filter and select

--- 01-data-wrangling.R
+++ 01-data-wrangling.R
@@ -5,8 +5,7 @@ rm(list = ls()) #Clean out workspace
 
 #datafile is ~13k lines long so let's just read in 2k
 ds <- read_csv(here("data_example_1","training_data.csv"), n_max = 2000)
-
-print(ds, n_extra = F)
+print(ds, n_extra = F) #n_extra makes it print a bit nicer
 
 ##### FACTORS --------- 
 
@@ -16,7 +15,10 @@ print(ds, n_extra = F)
 #store a vector of category numbers
 class_num <- c(1,2,3,4,5,6,7,8,9,10) # class_num <- 1:10
 #store a vector of category labels as strings
-class_lab <-c("upright", "walking", "prone", "crawling","held_walk","held_stat","sit_surf","sit_cg","sit_rest","supine")
+class_lab <- c("upright", "walking", "prone", "crawling","held_walk","held_stat","sit_surf","sit_cg","sit_rest","supine")
+
+class(class_num)
+class(class_lab)
 
 ds$class <- factor(ds$class, levels = class_num, labels = class_lab)
 ds$class_rel <- factor(ds$class_rel, levels = class_num, labels = class_lab)
@@ -30,49 +32,91 @@ ds$class <- fct_drop(ds$class)
 ds$class_rel <- fct_drop(ds$class_rel)
 fct_count(ds$class) #Note the change
 
-#Let's reshuffle some categories
+#Let's reorganize some factor levels
 ds$class <- fct_collapse(ds$class, 
-                         prone = "prone", supine = "supine",
-                         held = c("held_walk","held_stat"),
+                         prone = "prone", supine = "supine",         #these stay the same
+                         held = c("held_walk","held_stat"),          #levels listed here will be collapsed
                          sit = c("sit_surf", "sit_cg", "sit_rest"))
 ds$class_rel <- fct_collapse(ds$class_rel, 
                          prone = "prone", supine = "supine",
                          held = c("held_walk","held_stat"),
                          sit = c("sit_surf", "sit_cg", "sit_rest"))
 
-##### ARRANGE, FILTER, SELECT --------- 
+#Let's glance at some of the other options in "forcats"
+?forcats
+
+##### ARRANGE, FILTER, SELECT, USING LOGIC --------- 
+
+#### Arrange means sort ----- 
 
 #Let's sort our dataset by class (and then class_rel)
-ds <- ds %>% arrange(class, class_rel)
+ds %>% arrange(class, class_rel) #Did it stay that way? Yes
+#Remember, the pipe statement above is just a shorthand for:
+#arrange(ds, class, class_rel)
+
+#Let's sort our dataset by class (and then class_rel)
+ds <- ds %>% arrange(class, class_rel) #Did it stay that way? Yes
+ds %>% arrange(class, class_rel) -> ds2 #You can do this if it makes more sense to you
 
 #Helpful for looking for matches, but makes more sense to stay sorted by time
 ds <- ds %>% arrange(time)
 
 #Let's create a variable that checks whether class and class_rel match
-ds$match <- ds$class == ds$class_rel #logic of ==, what's the type of our new var?
-class(ds$match)
+ds$match <- ds$class == ds$class_rel 
+class(ds$match) #What type is match?
+
+#Applying logical operators
+1 == 1
+2 > 3
+"sit" == "sit"
+"sit" == "stand"
+
+#Works element-wise on vectors
+class_num_plus1 <- class_num + 1
+class_num  == class_num_plus1
+class_num  < class_num_plus1
 
 #Maybe a numeric variable would be better?
 ds$match <- ifelse(ds$class == ds$class_rel, 1, 0)
 class(ds$match)
-mean(ds$match)
+?ifelse #just like excel!
 
-#Let's filter to just look at the mismatches
-ds %>% filter(match == 0) #Note that ds hasn't changed, we didn't save anything
+#### Filter means choosing rows ----
 
-#Do we really need 306 variables right now? Let's select a subset of columns
-ds %>% filter(match == 0) %>% select(time:class_prop_rel)
+#Base R 
+subset(ds, match == 0)
+ds[ds$match == 0,]
 
-#Other select variants
-ds %>% filter(match == 0) %>% select(time, class, class_rel)
-ds %>% filter(match == 0) %>% select(c("time", "class", "class_rel"))
-ds %>% filter(match == 0) %>% select(time, starts_with("class"))
+#Tidyverse uses "filter" 
+ds %>% filter(match == 0)
 
-#Other types of logic for filters
-ds %>% filter(class_prop < 1) %>% select(class_prop) %>% arrange(class_prop)
-ds %>% filter(class_prop > .5 & class_prop < .6) %>% select(class_prop) %>% arrange(class_prop)
-ds %>% filter(class == "held" | class == "supine")
-ds %>% filter(class %in% c("held","supine"))
+#Other filtering examples
+ds %>% filter(class == "prone" | class == "supine") #Example of using "or" aka |
+ds %>% filter(class == "supine" & class_rel == "sit") #Example of using "and" aka &
+ds %>% filter(time > 319 & time < 322) #Example of using "and" with numeric ranges
+ds %>% filter(class == "prone") %>% filter(time > 319) %>% filter(match == 1) #endless chaining (same as &)
+ds %>% filter(class %in% c("held","supine")) #if class is in a list
+
+#### Select means choosing columns ---
+
+#Base R, select a single column
+m1 <- ds$match #returns vector
+#Tidyverse, select a single column
+m2 <- select(ds, match) #returns tibble
+#Tidyverse, pull a single column
+m3 <- pull(ds, match) #returns vector
+
+#Base R, select multiple columns
+ds[, c("match", "class")]
+ds[, time:class_rel] #no
+ds[, 1:4] #yes, but not very useful
+
+#Tidy select
+ds %>% select(time:class_rel) #range of adjacent columns
+ds %>% select(time, class, class_rel) #list of columns
+ds %>% select(c("time", "class", "class_rel")) #list of string column names
+ds %>% select(time, starts_with("class")) #starts_with, ends_with
+ds %>% select(contains("rel"))
 
 #filter, select, etc. all return tibbles, but aren't saved by default. 
 ds_mismatch <- ds %>% filter(match == 0) %>% select(time:class_prop_rel)
