6e53ad8b85cf980868adc2b55f47cd05f50d87dc
John Franchak <franchak@gmail.com>
2021-01-26 13:19:04 -0800

    updated wrangling examples

--- 03-data-pivotingwide_joining.R
+++ 03-data-pivotingwide_joining.R
@@ -22,9 +22,6 @@ header <- vroom(file_names, delim = " ", n_max = 8, col_names = c("field","value
 print(header)
 #One observation per row, and variables all in their own columns
 #"Field" currently has different variables all bunched in the same column
-#Any time you have a whole lot of different data types in a column (like field),
-#that's a cue to reshape from longer to wider format
-#We need to spread them out!
 
 #Time to pivot_wider
 #id_cols = column that is unique for each observation (in our case, the file name)
@@ -38,22 +35,15 @@ print(header)
 #We can't just add columns. Why not? Look at the # of observations in each tibble
 
 #left_join will merge datasets, and will fill the right number of rows if you can 
-#match each data frame by a key (in this case, file name)
+#match each data frame by a unique key (in this case, file name)
 ds_joined <- left_join(header, ds, by = "file")
 print(ds_joined)
-#ds_joined has the right # of observations (240), and pickd up all 9 variables from the header
+#ds_joined has the right # of observations (240), and picked up all 9 variables from the header
 
 #Let's use "separate" to clean up the file name into something more useful
 #Takes a character variable into multiple columns based on delimiters
 # "data_example_3/6191_1.txt" becomes "data" "example" "3" "6191" "1" "txt"
-#NA is saying "throw this out", we only want participant and block
+#NA is saying "throw this out", we only want participant and block parts of the filename
 ds_joined <- ds_joined %>% separate(file, into = c(NA,NA, NA, "Participant_file","Block_file", NA))
 ds_joined <- ds_joined %>% rename_with(make_clean_names) #handy fuction from janitor package
-
-speed_labels = c("slower", "faster")
-ds_joined <- ds_joined %>% 
-  mutate(
-    speed_actual = factor(speed_actual, levels = c("slo","fas"), labels = speed_labels),
-    speed_response = factor(speed_response, levels = c("slower","faster"), labels = speed_labels),
-  )
 
--- 04_pivotinglonger.R
+++ 04_pivotinglonger.R
@@ -4,7 +4,12 @@ library(tidyverse)
 rm(list = ls()) #clean variables out of environment
 
 ds <- read_csv('data_example_4/vocab.csv')
-
+#let's add some 'fixed' columns
+ds <- ds %>% mutate(
+  id = "Jonah",
+  dob = as.Date("2017-08-30"),
+  .before = "12"
+)
 print(ds)
 
 #Perfect example of "columns as values"
@@ -15,24 +20,8 @@ print(ds)
 #cols = which columns should be stacked
 #names_to = what to call the variable that will contain our former column names
 #values_to = what to call the variable that will contain the values
-ds_long <- pivot_longer(ds, cols = everything(), names_to = "age", values_to = "word")
+ds_long <- pivot_longer(ds, cols = -(id:dob), names_to = "age", values_to = "word")
 
 #Can drop NA values when pivoting
-ds_long <- pivot_longer(ds, cols = everything(), names_to = "age", values_to = "word", values_drop_na = T)
-
-#Can use tidy_select to choose which cols to select to stack 
-#Uunstacked columns are retained, which can lead to some strange output
-ds_long <- pivot_longer(ds, cols = ends_with(".5"), names_to = "age", values_to = "word", values_drop_na = T)
-
-#More realistic example
-
-#let's add some 'fixed' columns
-ds <- ds %>% mutate(
-  id = "Jonah",
-  dob = as.Date("2017-08-30"),
-  .before = "12"
-)
-
-#id/dob get repeated to match the new length of the data set
 ds_long <- pivot_longer(ds, cols = -(id:dob), names_to = "age", values_to = "word", values_drop_na = T)
-ds_long$age <- as.numeric(ds_long$age)
+
